3f43d07acbd16a60730ce636522c33404b19aa2d
kapil <kapildulbaji009@gmail.com>
2021-11-21 22:22:29 +0530

    absent present commit

--- EmpWage/EmpWage/Program.cs
+++ EmpWage/EmpWage/Program.cs
@@ -1,13 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace EmpWage
 {
-    class Program
+    class EmployeeUseCase1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("hello");
+            int isPresent = 1;
+            Random random = new Random();
+            int randominput = random.Next(0, 2);
+
+            if (randominput == isPresent)
+            {
+                Console.WriteLine("Present");
+            }
+            else
+            {
+                Console.WriteLine("Absent");
+            }
+            Console.ReadLine();
         }
     }
+
 }
